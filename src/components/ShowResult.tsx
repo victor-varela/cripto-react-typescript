c3c6d4477ec65c490a1feca10bb553e4cc316e9f
Victor Varela <victor398@gmail.com>
2025-08-05 12:23:49 -0300

    feat: add FontAwesome icons to ShowResult component for visual representation of price changes

--- src/components/ShowResult.tsx
+++ src/components/ShowResult.tsx
@@ -1,37 +1,52 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowUp, faArrowDown } from "@fortawesome/free-solid-svg-icons";
 import { useCriptoStore } from "../store";
 import Spinner from "./Spinner";
 
 //Lo que hiciste se llama indexed access type. el uso de corchetes...
 const ShowResult = () => {
   const result = useCriptoStore(set => set.result);
-  const loading = useCriptoStore(set=> set.loading);
+  const loading = useCriptoStore(set => set.loading);
   const hasResult = result.VALUE > 0;
-  
+
+  const isPositive = result.CURRENT_DAY_CHANGE_PERCENTAGE > 0;
+
   return (
     <div className="result-wrapper">
-      { loading ? <Spinner/> : hasResult && (
-        <>
-          <h2>Cotizacion</h2>
-          <div className="result">
-            <div className="result__img">
-              <img src="./criptos-img.jpg" alt="Imagen criptomonedas" />
+      {loading ? (
+        <Spinner />
+      ) : (
+        hasResult && (
+          <>
+            <h2>Cotizacion</h2>
+            <div className="result">
+              <div className="result__img">
+                <img src="./criptos-img.jpg" alt="Imagen criptomonedas" />
+              </div>
+              <div className="info">
+                <p>
+                  El precio es de: <span>${result.VALUE.toLocaleString()}</span>
+                </p>
+                <p>
+                  <p>
+                    Precio mas alto del dia: <span>${result.CURRENT_DAY_HIGH.toLocaleString()}</span>
+                  </p>
+                  Precio mas bajo del dia: <span>${result.CURRENT_DAY_LOW.toLocaleString()}</span>
+                </p>
+                <p>
+                  La variacion del dia es:{" "}
+                  <span>
+                    {result.CURRENT_DAY_CHANGE_PERCENTAGE.toLocaleString()}%
+                    <FontAwesomeIcon
+                      icon={isPositive ? faArrowUp : faArrowDown}
+                      style={{ marginLeft: "0.5rem", color: isPositive ? "green" : "red" }}
+                    />
+                  </span>
+                </p>
+              </div>
             </div>
-            <div className="info">
-              <p>
-                El precio es de: <span>${result.VALUE.toLocaleString()}</span>
-              </p>
-              <p>
-                Precio mas bajo del dia: <span>${result.CURRENT_DAY_LOW.toLocaleString()}</span>
-              </p>
-              <p>
-                Precio mas alto del dia: <span>${result.CURRENT_DAY_HIGH.toLocaleString()}</span>
-              </p>
-              <p>
-                La variacion del dia es: <span>{result.CURRENT_DAY_CHANGE_PERCENTAGE.toLocaleString()}%</span>
-              </p>
-            </div>
-          </div>
-        </>
+          </>
+        )
       )}
     </div>
   );
